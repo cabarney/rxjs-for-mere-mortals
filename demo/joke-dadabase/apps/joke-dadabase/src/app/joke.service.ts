c3513a6fdf6d3115cad374477a57dfe7654f40ea
Barney, Adam <adambarney@rocketmortgage.com>
2022-07-15 14:44:24 -0500

    nebraska.code() demos

--- demo/joke-dadabase/apps/joke-dadabase/src/app/joke.service.ts
+++ demo/joke-dadabase/apps/joke-dadabase/src/app/joke.service.ts
@@ -1,21 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Joke, JokeResponse, PagedQuery } from '@joke-dadabase/api-interfaces';
-import { BehaviorSubject,forkJoin, of, Subject } from 'rxjs';
-import { map, mergeMap, tap, switchMap } from 'rxjs/operators';
+import { JokeResponse, PagedQuery } from '@joke-dadabase/api-interfaces';
+import { BehaviorSubject } from 'rxjs';
+import { map, switchMap, share } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokeService {
   url = "http://localhost:3333/api";
-
+  _querySubject: BehaviorSubject<PagedQuery> = new BehaviorSubject<PagedQuery>({ pageIndex: 0, pageSize: 10, category: '', searchTerm: '' });
+  querySubject$ = this._querySubject.asObservable();
   constructor(private http: HttpClient) { }
-
+  _loadJokes$ = this.querySubject$.pipe(
+    switchMap(query => this.http.get<JokeResponse>(`${this.url}/jokes?${this.toQueryString(query)}`)),
+    share()
+  );
+  public jokes$ = this._loadJokes$.pipe(map(response => response.result));
+  public totalJokes$ = this._loadJokes$.pipe(map(response => response.total));
   public getJokes(query: PagedQuery) {
-    return this.http.get<JokeResponse>(`${this.url}/jokes?${this.toQueryString(query)}`);
+    this._querySubject.next(query);
   }
-
   toQueryString(query: PagedQuery) {
     return `pageIndex=${query.pageIndex}&pageSize=${query.pageSize}&category=${query.category}&searchTerm=${query.searchTerm}`;
   }
--- demo/joke-dadabase/apps/joke-dadabase/src/app/jokes/jokes.component.ts
+++ demo/joke-dadabase/apps/joke-dadabase/src/app/jokes/jokes.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Joke, JokeResponse } from '@joke-dadabase/api-interfaces';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PagedQuery } from '@joke-dadabase/api-interfaces';
+import { fromEvent, Subscription } from 'rxjs';
+import { filter, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { JokeService } from '../joke.service';
 
 @Component({
@@ -7,13 +9,26 @@ import { JokeService } from '../joke.service';
   templateUrl: './jokes.component.html',
   styleUrls: ['./jokes.component.scss']
 })
-export class JokesComponent implements OnInit {
-  jokes: Joke[] = [];
-
+export class JokesComponent implements OnInit, OnDestroy {
+  jokes$ = this.service.jokes$;
+  total$ = this.service.totalJokes$;
+  query: PagedQuery = { pageIndex: 0, pageSize: 10, category: '', searchTerm: '' };
+  subscription: Subscription | undefined;
   constructor(private service: JokeService) { }
-
   ngOnInit(): void {
-    this.service.getJokes({ pageIndex: 0, pageSize: 10, category: '', searchTerm: '' })
-      .subscribe((jokes: JokeResponse) => this.jokes = jokes.result);
+    const searchBox = document.getElementById('search-box') as HTMLInputElement;
+    this.subscription = fromEvent(searchBox, 'input').pipe(
+      map(e => (e.target as HTMLInputElement).value),
+      filter(value => !value || value.length > 2),
+      debounceTime(400),
+      distinctUntilChanged(),
+      map(searchTerm => ({ ...this.query, searchTerm } as PagedQuery)),
+    ).subscribe(query => this.service.getJokes(query));
+    this.service.getJokes({ pageIndex: 0, pageSize: 10, category: '', searchTerm: '' });
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
